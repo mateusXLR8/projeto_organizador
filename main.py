6f5aadf84b2a89dd62cf9f533ac561028f394b31
mateus <mateuspedroso.senai@gmail.com>
2026-03-18 23:26:05 +0000

    passos de formiga

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from pathlib import Path
 import shutil
 from datetime import datetime
 
-shutil.unpack_archive('organizador.zip', 'arquivos')
+if not Path("arquivos").exists():
+    shutil.unpack_archive('organizador.zip', 'arquivos')
 
-for arquivo in Path("arquivos").glob("*.pdf"):
+for arquivo in Path("arquivos").glob("*"):
+    if arquivo.glob("*.pdf"):
+        pdf = Path("arquivos/pdf")
+        pdf.mkdir(parents=True, exist_ok=True)
+        shutil.move(arquivo, pdf)
+    
+
     
