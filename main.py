21c3d60369a2033b9ef18522973ac09488703be6
mateus <mateuspedroso.senai@gmail.com>
2026-03-20 23:45:21 +0000

    eu sou um genio? ou não...

--- main.py
+++ main.py
@@ -2,14 +2,30 @@ from pathlib import Path
 import shutil
 from datetime import datetime
 
-if not Path("arquivos").exists():
-    shutil.unpack_archive('organizador.zip', 'arquivos')
+if not Path("organizador").exists():
+    shutil.unpack_archive('organizador.zip', 'organizador')
 
-for arquivo in Path("arquivos").glob("*"):
-    if arquivo.glob("*.pdf"):
-        pdf = Path("arquivos/pdf")
-        pdf.mkdir(parents=True, exist_ok=True)
-        shutil.move(arquivo, pdf)
+if not Path("arquivos_organizados").exists():
+    arquivos_organizados = Path("arquivos_organizados")
+    arquivos_organizados.mkdir(exist_ok=True)
+
+arquivos_movidos = 0
+extensao_encontrada = []
+for arquivo in Path("organizador").iterdir():
+    extensao = arquivo.suffix.lower().replace(".", "")
+    pasta_destino = arquivos_organizados/extensao
+    pasta_destino.mkdir(exist_ok=True)
+    shutil.move(str(arquivo), str(pasta_destino))
+    arquivos_movidos += 1
+    extensao_encontrada.append(extensao)
+    hora = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    with open("registro.log", "a", encoding='utf-8') as registro:
+        registro.write(f"{hora}, Nome do arquivo: {arquivo}, Destino final: {pasta_destino}\n")
+
+print(f"Arquivos organizados: {arquivos_movidos}\nExtensões encontradas: {extensao_encontrada}")
+
+
+
     
 
     
